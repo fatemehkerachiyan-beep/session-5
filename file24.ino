8198f9c5f6d571ebe46072208f833b17b8e5621c
fatemeh <fatemehkerachiyan@gmail.com>
2025-11-14 15:14:24 +0330

    Add files via upload

---
+++ file24.ino
@@ -0,0 +1,17 @@
+int sensor;
+int led=9;
+
+void setup() {
+ Serial.begin(9600);
+ pinMode(led,OUTPUT);
+}
+
+void loop() {
+  sensor=analogRead(A1);
+  int brightness=map(sensor,0,500,255,0);
+  analogWrite(led,brightness);
+  Serial.print("Sensor Value:");
+  Serial.print(sensor);
+  Serial.print("          LED Brightness:");
+  Serial.println(brightness);
+}
---
+++ file25.ino
@@ -0,0 +1,20 @@
+#include "pitches.h"
+int mobile_notes[] = {NOTE_C4, NOTE_DS4, NOTE_G4, NOTE_C4, NOTE_DS4, NOTE_G4, NOTE_F4, NOTE_DS4, NOTE_F4, NOTE_DS4};
+int mobile_durations[] = {8, 8, 4, 8, 8, 8, 8, 16, 16, 2};
+
+void setup() {
+  mobile();
+}
+
+void loop() {
+}
+
+void mobile() {
+  for ( int thisNote = 0; thisNote < 10; thisNote++) {
+    int mobile_duration = 1000 / mobile_durations[thisNote];
+    tone(9, mobile_notes[thisNote], mobile_duration);
+    //Ta'Ayiine Meghdare Tempo Ahang
+    int pauseBetweenNotes = mobile_duration * 1.43;
+    delay(pauseBetweenNotes);
+  }
+}
